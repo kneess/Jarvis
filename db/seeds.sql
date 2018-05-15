8b32b18bf30ad2e6f43f1d98f79edb93eb96e917
knees <you@example.com>
2018-05-14 21:02:33 -0700

    seeds.sql file and sync

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO hospitals (hospital_name, zip_code, procedure, cost) VALUES ('sundried',85001,"knee joint",1200);
+INSERT INTO hospitals (hospital_name, zip_code, procedure, cost) VALUES ('prime',53075,'hip joint',8900);
+INSERT INTO hospitals (hospital_name, zip_code, procedure, cost) VALUES ('Sylvester',96815,'umbilical joint',90000);
