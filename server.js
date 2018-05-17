7a5df478a2368f2fb3664c89571e34e4ce96f5ea
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-05-16 17:41:38 -0700

    Remove force true from sync so that the Table does not drop

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(routes);
  
 // });
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
